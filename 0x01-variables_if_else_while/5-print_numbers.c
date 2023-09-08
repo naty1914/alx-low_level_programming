afd1223601076aa62cdee9bbc0d1c223ce0ae010
Natnael_Seifu <natiseifu02@gmail.com>
2023-09-08 15:37:24 -0700

    complete from task 4 upto task 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; l++)
+	{
+		printf("%d", l);
+	}
+	printf("\n");
+	return (0);
+}
