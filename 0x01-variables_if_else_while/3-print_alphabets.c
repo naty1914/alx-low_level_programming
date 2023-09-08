4cdd5e7ae1b0d43e486955ae39aa6af383fae87d
Natnael_Seifu <natiseifu02@gmail.com>
2023-09-08 14:52:25 -0700

    comlete form task 0 upto task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char l;
+	char L;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (L = 'A'; L <= 'Z'; L++)
+	{
+		putchar(L);
+	}
+	putchar('\n');
+	return (0);
+}
