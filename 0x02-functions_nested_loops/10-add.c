8bb08445e94539a1b228498e01f0866162eae753
Natnael_Seifu <natiseifu02@gmail.com>
2023-09-13 15:02:07 -0700

    complete form task 6 upto task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:sum of a and b
+ */
+
+
+int add(int a, int b)
+{
+return (a + b);
+}
