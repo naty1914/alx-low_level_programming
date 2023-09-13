55627d695bd2859af15c32cbdc194da00ae8cac0
Natnael_Seifu <natiseifu02@gmail.com>
2023-09-13 13:53:48 -0700

    complete from task 1 upto task 5

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+  * Return: Always 
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
