1a71b98985f1378363e3a66705f8db70994fcd09
Natnael_Seifu <natiseifu02@gmail.com>
2023-09-15 16:47:45 -0700

    complete task 11 an task 12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+long number = 612852475143;
+int i;
+while (i++ < number / 2)
+{
+if (number % i == 0)
+{
+number /= 2;
+continue;
+}
+
+for (i = 3; i < number / 2; i += 2)
+{
+if (number % i == 0)
+number /= i;
+
+}
+}
+printf("%ld\n", number);
+return (0);
+}
