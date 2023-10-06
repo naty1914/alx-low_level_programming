50674a2dbedec6fe67d949e418b6437bc4179aee
Natnael_Seifu <natiseifu02@gmail.com>
2023-10-06 16:15:18 -0700

    complete task 4 and task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _print(char *str, int l);
-int check_for_digits(char **av);
-void init(char *str, int l);
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -11,6 +8,9 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
