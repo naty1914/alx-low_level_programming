050c04a8a7b925311662143ab5fb2baec4a1e4d6
Natnael_Seifu <natiseifu02@gmail.com>
2023-10-09 16:34:10 -0700

    complete from task 1 upto task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free struct pointer
+ *
+ * @d: struct dog to free
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
