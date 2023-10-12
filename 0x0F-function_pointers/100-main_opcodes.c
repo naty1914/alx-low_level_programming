bce4ec52e17decd830663019e6fb67cb462527b2
Natnael_Seifu <natiseifu02@gmail.com>
2023-10-11 17:31:09 -0700

    complete last task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main -  prints the opcodes of its own main function
+ *@argc: argument counter
+ *@argv: argument vector
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx ", ((char *)main)[i]);
+	printf("%02hhx\n", ((char *)main)[i]);
+	return (0);
+}
