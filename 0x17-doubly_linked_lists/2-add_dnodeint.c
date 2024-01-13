4961f49988d5079c09a1dff42d90601428d57515
Natnael Seifu <natiseifu02@gmail.com>
2024-01-12 18:09:55 -0800

    second commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,21 +1,32 @@
 #include "lists.h"
 
+
+
+
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
  dlistint_t *ptr;
 ptr =malloc(sizeof( dlistint_t));
-if (ptr == NULL)
-    {
-  
-        return NULL;
-    }
 
 ptr->n = n;
-ptr->next = NULL;
-ptr->prev = NULL;
-ptr->next =*head;
 
-*head = ptr;
+ptr->prev = NULL;
+
+
+ if (*head == NULL)
+    {
+        
+        ptr->next = NULL;
+    }
+    else
+    {
+    
+        ptr->next = *head;
+        (*head)->prev = ptr;
+    }
+
+*head= ptr;
 return ptr;
 
 
