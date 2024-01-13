d4953b88cb9870ac29a57eb489edd8d1f15f74a6
Natnael Seifu <natiseifu02@gmail.com>
2024-01-12 17:42:33 -0800

    second commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct dlistint_s
-{
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
+#include "lists.h"
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
